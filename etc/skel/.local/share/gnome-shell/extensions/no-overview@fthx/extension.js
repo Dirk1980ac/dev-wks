421de9615f2d42ad52d51f3d953c9cf821fb36bf
Dirk Gottschalk <dirk.gottschalk1980@googlemail.com>
2025-07-06 01:39:17 +0200

    Pre-install favourite extensions for all users.

---
+++ etc/skel/.local/share/gnome-shell/extensions/no-overview@fthx/extension.js
@@ -0,0 +1,19 @@
+/*
+  No overview at start-up
+  GNOME Shell 45+ extension
+  Contributors: @fthx
+  License: GPL v3
+*/
+
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+export default class NoOverviewExtension {
+    enable() {
+        if (Main.layoutManager._startingUp)
+            Main.layoutManager.connectObject('startup-complete', () => Main.overview.hide(), this);
+    }
+
+    disable() {
+        Main.layoutManager.disconnectObject(this);
+    }
+}
